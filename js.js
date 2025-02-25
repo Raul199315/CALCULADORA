b7d525802d591c08baaf103185835f0bfbc14078
Raul Blanco <blancogil199@gmail.com>
2025-02-25 18:50:55 -0500

    add funcion borrar

--- js.js
+++ js.js
@@ -1,6 +1,14 @@
+// variables 
+let numerosEnPantalla ;
+let numerosDijitados ;
+let ultimoNumero = [];
+
+
+
 function dijitarNumero(valor) {
     let pantalla = document.getElementById("pantalla");
     pantalla.value += valor;
+    return valor;
 }
 
 // funcion para borrar
@@ -24,6 +32,20 @@ function calcularResultado() {
 function tema(){
     let opciones = document.querySelector('selec').value;
     if(opciones == "claro" ){
-        
+    
     }
+}
+
+function capturarUtimoNumeor(){
+        // Obtener el elemento de entrada por su id
+        let input = document.getElementById('pantalla');
+    
+        // Obtener el valor actual del input
+        let valor = input.value;
+    
+        // Eliminar el último carácter (en este caso, número) del valor del input
+        let nuevoValor = valor.slice(0, -1);
+    
+        // Asignar el nuevo valor al input
+        input.value = nuevoValor;
 }
